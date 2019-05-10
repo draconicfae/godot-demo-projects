dc60b31c603f75ff369e4bfd916d2712cc92ad56
Jamie Campbell <jamie@parit.ca>
2019-05-09 21:53:18 -0700

    added some kind of dumb but functional preliminary free camera controls

--- 3d/truck_town/follow_camera.gd
+++ 3d/truck_town/follow_camera.gd
@@ -10,12 +10,14 @@ export var autoturn_ray_aperture = 25
 export var autoturn_speed = 50
 var max_height = 2.0
 var min_height = 0
+var following_camera = true
+var up = Vector3(0, 1, 0)
+var target
+var pos
 
-
-func _physics_process(dt):
-	var target = get_parent().get_global_transform().origin
-	var pos = get_global_transform().origin
-	var up = Vector3(0, 1, 0)
+func do_camera_follow():
+	target = get_parent().get_global_transform().origin
+	pos = get_global_transform().origin
 	
 	var delta = pos - target
 	
@@ -41,6 +43,27 @@ func _physics_process(dt):
 	var t = get_transform()
 	t.basis = Basis(t.basis[0], deg2rad(angle_v_adjust))*t.basis
 	set_transform(t)
+	
+func do_freecamera():
+	if Input.is_action_just_pressed("ui_up"):
+		pos.y = pos.y + 1
+	if Input.is_action_just_pressed("ui_down"):
+		pos.y = pos.y -1
+	if Input.is_action_just_pressed("ui_left"):
+		target.x = target.x -1
+	if Input.is_action_just_pressed("ui_right"):
+		target.x = target.x + 1
+		
+	look_at_from_position(pos, target, up)
+	
+func _physics_process(dt):
+	if Input.is_action_just_pressed("ui_camerafollow"):
+		following_camera = !following_camera
+		
+	if following_camera:
+		do_camera_follow()
+	else:
+		do_freecamera()
 
 
 func _ready():
