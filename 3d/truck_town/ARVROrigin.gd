edec5daf38c715cf21a02e25368711c311e7a68f
Jamie Campbell <jamie@parit.ca>
2020-10-26 18:49:06 -0700

    changed to run in virtual reality

---
+++ 3d/truck_town/ARVROrigin.gd
@@ -0,0 +1,19 @@
+extends ARVROrigin
+
+var interface : ARVRInterface
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var interface = ARVRServer.find_interface("Native mobile")
+	if interface and interface.initialize():
+		get_viewport().arvr = true	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
